91306fa2210716101ced5e429f79ede56785bb2c
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2025-03-23 02:09:59 +0600

    server deploy on render vercel foka websocket support korena vuya vuya modhu modhu

--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState, useRef } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 import { io } from "socket.io-client";
 
-const socket = io("https://chap-app-backend-iota.vercel.app/");
+const socket = io("https://chap-app-1-yyt0.onrender.com/");
 
 export default function Chat() {
   const router = useRouter();
